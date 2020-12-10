4419ba862626bc42dfa9edd122f8c589e034a9fc
Jan Pabisiak <ryze4cs@gmail.com>
2020-12-10 21:07:01 +0100

    Delete footer.php

--- core/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <?php echo date("Y"); ?> Copyright &copy; | <a href="https://janpabisiak.pl">Jan Pabisiak</a> | JProber v1.0
-            </div>
-        </div>
-    </div>
-</footer>
